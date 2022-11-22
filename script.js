a23c794f78557715596db87731f1686dbfb8eda2
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-22 17:47:48 +0100

    Added working script with prompt input

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const km = parseInt(prompt("Inserisci quanti kilometri vuoi fare"));
+/*const inputDistance = document.querySelector(parseInt('input#distance'))
+const inputAge = document.querySelector(parseInt('input#age'))
+*/
+const distance = parseInt(prompt("Inserisci quanti kilometri vuoi fare"));
 const age = parseInt(prompt("Inserisci la tua età"));
+const button = document.querySelector('button')
 
-let price = km * 0.21;
+button.addEventListener('click', function(){
+    let price = distance * 0.2762;
+    if (age < 18){
+        price = price - ((price / 100) * 17.5)
+    } else if (age > 65){
+        price = price - ((price / 100) * 33.3)
+    }
+    console.log(price)
+})
 
-if (age < 18){
-    price = price - ((price / 100) * 20)
-} else if (age > 65){
-    price = price - ((price / 100) * 40)
-}
-
-document.getElementById('price').innerHTML = price.toFixed(2) + '&euro;';
+    
+/*
+console.log(inputDistance.value)
+console.log(inputAge.value)
+*/
