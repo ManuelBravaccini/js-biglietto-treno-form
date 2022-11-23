857e62f8dee83aec5dd6fc15890d5a4dbc258cf6
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-23 10:20:49 +0100

    Moved const "distance" and "age" in EventListener

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const distance = parseInt(prompt("Inserisci quanti kilometri vuoi fare"));
 const age = parseInt(prompt("Inserisci la tua età"));
 */
-const distance = document.querySelector(parseInt('input#distance'))
-const age = document.querySelector(parseInt('input#age'))
 const button = document.querySelector('button')
 
 button.addEventListener('click', function(){
+    const distance = document.querySelector('input#distance').value
+    const age = document.querySelector('input#age').value
     let price = distance * 0.2762;
     if (age < 18){
         price = price - ((price / 100) * 17.5)
     } else if (age > 65){
         price = price - ((price / 100) * 33.3)
     }
-    console.log(price)
+    /*console.log(price)*/
     document.getElementById('price').innerHTML = price.toFixed(2) + '&euro;';
 })
 
